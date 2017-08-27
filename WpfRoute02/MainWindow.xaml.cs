baef2e4d07998b17b9a4d44b728ea0efba342225
klboyer1 <klboyer1@gmail.com>
2017-08-27 01:21:42 -0400

    Added fileteed DataGrid data -- Need to fix the Updated field (Datetime field)

--- WpfRoute02/MainWindow.xaml.cs
+++ WpfRoute02/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ using System.Windows.Shapes;
 
 namespace WpfRoute02
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -23,11 +25,37 @@ namespace WpfRoute02
         public MainWindow()
         {
             InitializeComponent();
+            FillStreetBox();
         }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+        public void FillStreetBox()
+        {
+            SqlControl st = new SqlControl();
+            string Query = "Select Street from Streets";
+            st.ExecQuery(Query);
+
+            foreach (DataRow dr in st.DT.Rows)
+            {
+                cboxStreet.Items.Add(dr["Street"].ToString());
+            }
+
+        }
+        private void cboxStreet_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            SqlControl st = new SqlControl();
+            string Query = "Select Street, HouseNum, Unit, Code, TVGuide from Route02 where Street = " +'"'+ cboxStreet.SelectedItem.ToString() + '"';
+            st.ExecQuery(Query);
+            DataGrid1.ItemsSource = st.DT.DefaultView;
+           
         }
     }
 }
---
+++ WpfRoute02/SqlControl.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+
+namespace WpfRoute02
+{
+    class SqlControl
+    {
+        public String cs = @"Data Source=C:\Users\klboy\Google Drive\Projects\WpfRoute02\WpfRoute02\AppData\WpfRoute02.db;";
+
+        // QUERY STATISTICS
+        public int RecordCount;
+        public DataTable DT;
+
+        // EXECUTE QUERY SUB
+        public void ExecQuery(String Query)
+        {
+            // RESET QUERY STATS
+            RecordCount = 0;
+
+            try
+            {
+
+                // CREATE DB COMMAND
+                SQLiteConnection conn = new SQLiteConnection(cs);
+                SQLiteCommand Cmd = new SQLiteCommand(Query, conn);
+                SQLiteDataAdapter DA = new SQLiteDataAdapter(Cmd);
+                DT = new DataTable();
+
+                // EXECUTE COMMAND & FILL DATASET
+                RecordCount = DA.Fill(DT);
+                DA.Update(DT);
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+                throw;
+            }
+        }
+        // EXECUTE NON QUERY SUB { insert delete }
+        public void ExecNonQuery(String Query)
+        {
+            SQLiteConnection conn = new SQLiteConnection(cs);
+            try
+            {
+                conn.Open();
+                SQLiteCommand Cmd = new SQLiteCommand(Query, conn);
+                Cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}    
+
+// *******************************
